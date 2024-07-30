bcfe80315ca99105f903b858e569494ea451ab03
wrog0003 <wrog0003@student.monash.edu>
2024-07-30 08:45:59 +0100

    motor test

--- GPIO_test.py
+++ GPIO_test.py
@@ -2,8 +2,11 @@
 from gpiozero import Button, LED
 from time import sleep as sl
 out = LED(17)
+out2 = LED(27)
 while True:
+    out2.off()
     out.on()
     sl(0.5)
     out.off()
+    out2.on()
     sl(0.5)
