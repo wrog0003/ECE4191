cc466886ec86a58432f034e2b3442b12080e704e
wrog0003 <82739636+wrog0003@users.noreply.github.com>
2024-08-01 07:23:53 +1000

    started vision

---
+++ enums.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class STATE (Enum):
+    unloading = 1
+    return2Home = 2 #use for milestone 1
+    turnAround =3 
+    turn2Ball = 4 # use for milestone 1
+    move2Ball = 5 # use for milestone 1 
+
+
+    
+    
--- opencv_test.py
+++ opencv_test.py
@@ -1,11 +1,31 @@
-print("hello world")
+'''
+Based of of https://pyimagesearch.com/2015/09/14/ball-tracking-with-opencv/
+'''
 import cv2 as cv
+
+print("hello world")
+
+# define HSV values
+greenLower = (29, 86, 6)
+greenUpper = (64, 255, 255)
 
 cam = cv.VideoCapture(0)
 result, image = cam.read()
 if result:
     print("success")
     #cv.imshow("GeeksForGeeks", image)
+    # resize image and apply mask
+    [x,y]= image.shape[:2]
+    print(x,y)
+    frame = cv.resize(image,(round(x/2),round(y/2)))
+    blurred = cv.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv.cvtColor(blurred, cv.COLOR_BGR2HSV)
+    mask = cv.inRange(hsv, greenLower, greenUpper)
+    # remove artefacts/ stray pixels
+    mask = cv.erode(mask, None, iterations=2)
+    mask = cv.dilate(mask, None, iterations=2)
+    cv.imshow(mask)
+
     cv.imwrite("GeeksForGeeks.png", image)
 else :
     print("issues")
