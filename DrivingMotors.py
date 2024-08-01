8490e433549de3092e1006ad0874a921697f0bf2
EVladicic <evla0003@student.monash.edu>
2024-08-01 11:23:44 +1000

    Update DrivingMotors.py

--- DrivingMotors.py
+++ DrivingMotors.py
@@ -31,11 +31,19 @@ motor1b = 27
 motor2a = 23
 motor2b = 24
 
+# Set up GPIO pins
 GPIO.setmode(GPIO.BCM)
+
 GPIO.setup(motor1a, GPIO.OUT) 
 GPIO.setup(motor1b, GPIO.OUT)
 GPIO.setup(motor2a, GPIO.OUT)
 GPIO.setup(motor2b, GPIO.OUT) 
+
+# set the GPIO pins as PWM 
+pwm1a = GPIO.PWM(motor1a,1000)
+pwm1b = GPIO.PWM(motor1b,1000)
+pwm2a = GPIO.PWM(motor2a,1000)
+pwm2b = GPIO.PWM(motor2b,1000)
 
 def backwards(duty_cycle, stop_cmd):
     #Duty cycle between 0-100
@@ -59,3 +67,53 @@ def backwards(duty_cycle, stop_cmd):
     
     
 backwards(0.5,0)
+
+def fowards(duty_cycle, stop_cmd):
+ 
+    # duty cycle between 0 - 100
+
+    while stop_cmd !=1:
+        
+        # drive the motor forwards 
+        pwm1a.start(0)
+        pwm1b.start(duty_cycle)
+        pwm2a.start(0)
+        pwm2b.start(duty_cycle)
+
+    pwm1a.stop()
+    pwm1b.stop()
+    pwm2a.stop()
+    pwm2b.stop()
+
+
+def left(duty_cycle, stop_cmd):
+    # duty cycle between 0 - 100
+
+    while stop_cmd !=1:
+        
+        # drive the motor forwards 
+        pwm1a.start(0)
+        pwm1b.start(duty_cycle)
+        pwm2a.start(0)
+        pwm2b.start(0)
+
+    pwm1a.stop()
+    pwm1b.stop()
+    pwm2a.stop()
+    pwm2b.stop()
+
+def right(duty_cycle, stop_cmd):
+    # duty cycle between 0 - 100
+
+    while stop_cmd != 1:
+
+        # drive the left motor forward 
+        pwm1a.start(0)
+        pwm1b.start(0)
+        pwm2a.start(0)
+        pwm2b.start(duty_cycle)
+
+    pwm1a.stop()
+    pwm1b.stop()
+    pwm2a.stop()
+    pwm2b.stop()
