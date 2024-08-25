1c5ba86f9cf40c496414328621fd7e28d40bcb5e
wrog0003 <82739636+wrog0003@users.noreply.github.com>
2024-08-25 12:17:54 +1000

    Fixing system 5

--- Sys5_Contol_M1.py
+++ Sys5_Contol_M1.py
@@ -1,11 +1,11 @@
 # overall brains script for robot, written by Warren Rogan 
 
 from ECE4191enums import STATE, DIRECTION, ACTION
-from Sys3_SLAM import Sys3_SLAM
+from Motor_code.encoderClass import SimpleEncoder
 from Sys4_Vision import Sys4_Vision
 
 from time import sleep
-from math import sqrt, atan2, pi
+from math import pi, atan2, sqrt, sin, cos
 
 import RPi.GPIO as GPIO
 import GLOBALSM1 #for physical values 
@@ -54,10 +54,18 @@ class Sys5_Control:
         self.pwm2b = GPIO.PWM(motor2b,1000)
 
         # create class instances 
+        # Ball finding 
         self.vision = Sys4_Vision()
-        self.location = Sys3_SLAM() 
+
+        #Position control
+        self.x_pos =0
+        self.y_pos = 0
+        self.rot =0 
+        self.State = None 
+        self.EncoderL = SimpleEncoder(motor1cha,motor1chb) # set up Left Motor
+        self.EncoderR = SimpleEncoder(motor2cha,motor2chb) # set up right Motor
     
-    def forwards(self,duty_cycle:float)->ACTION:
+    def _forwards(self,duty_cycle:float)->ACTION:
     
         # input: duty cycle between 0 - 100
         # output: return the state of the robot
@@ -70,7 +78,7 @@ class Sys5_Control:
 
         return ACTION.FORWARD
 
-    def backwards(self,duty_cycle:float)->ACTION:
+    def _backwards(self,duty_cycle:float)->ACTION:
     
         # input: duty cycle between 0 - 100
         # output: return the state of the robot
@@ -83,7 +91,7 @@ class Sys5_Control:
 
         return ACTION.BACKWARD 
 
-    def turn(self,duty_cycle:float,clockWise:bool)->ACTION:
+    def _turn(self,duty_cycle:float,clockWise:bool)->ACTION:
 
         # input: duty cycle between 0 - 100, if you want to turn clockwise or anticlockwise 
         # output: return the state of the robot
@@ -101,12 +109,149 @@ class Sys5_Control:
             self.pwm2b.start(max(duty_cycle*duty_cycle_bias,5))
             return ACTION.LEFT
 
-    def stop(self)->None:
+    def _stop(self)->None:
         self.pwm1a.stop()
         self.pwm1b.stop()
         self.pwm2a.stop()
         self.pwm2b.stop()
 
+    #Manual exit function to prevent loss of pin control
+    def _exemptExit(self)->None:
+        self._stop()
+        GPIO.cleanup()
+        self.EncoderL.end()
+        self.EncoderR.end()
+    
+    #Position tracking 
+    def _updatePos(self)->list[float]:
+        # get data
+        [newL, dirL, oldL] = self.encoderL.getValues()
+        [newR, dirR, oldR] = self.encoderR.getValues()
+
+        #difference
+        delL = abs(newL-oldL)
+        delR = abs(newR-oldR)
+        #print(delL-delR)
+        #get average travelled distance 
+        distanceAvg = ((delL*GLOBALSM1.distancePerPulse)+(delR*GLOBALSM1.distancePerPulse))/2 
+        x = 0
+        y = 0 
+        rot = 0 
+        #determine direction
+        if self.State == ACTION.FORWARD:
+            rot = self.rot
+            y= self.y_pos+(distanceAvg*sin(rot*pi/180))
+            x = self.y_pos+(distanceAvg*cos(rot*pi/180))
+        elif self.State ==ACTION.BACKWARD:
+            rot = self.rot
+            y= self.y_pos-(distanceAvg*sin(rot*pi/180))
+            x = self.x_pos-(distanceAvg*cos(rot*pi/180))
+        elif self.State == ACTION.LEFT:
+            x= self.x_pos
+            y = self.y_pos
+            delAngle = distanceAvg*360/GLOBALSM1.wheelBaseCircumference
+            rot = self.rot+delAngle
+        elif self.State == ACTION.RIGHT:
+            x= self.x_pos
+            y = self.y_pos 
+            delAngle = distanceAvg*360/GLOBALSM1.wheelBaseCircumference
+            rot = self.rot-delAngle
+        return x,y,rot
+    #Calibration test to check that the robot goes in the right direction
+    def CalibrationTest(self)->None:
+        try:
+            self._forwards(30)
+            sleep(1)
+            self._backwards(30)
+            sleep(1)
+            self._turn(30,ANTICLOCKWISE)
+            sleep(1)
+            self._turn(30,CLOCKWISE)
+            sleep(1)
+            self._stop() 
+            print("Test run")
+            GPIO.cleanup()
+    # clear up if exited manually
+        except KeyboardInterrupt:
+            self._exemptExit()
+    #release pins
+    def release(self)->None:
+        GPIO.cleanup()
+    #Simple goto
+    def GoTo(self,X:float,Y:float,speed:float = 30)->None:
+        
+        # get angle to point
+        angle = atan2(Y,X)*180/pi
+        distance = GLOBALSM1.wheelBaseCircumference*abs(angle)/360 # get the distance that needs to be travelled to 
+        numPulses = distance/GLOBALSM1.distancePerPulse
+        try:
+            # rotate 
+            if (angle >-1 and angle <1): # no rotation required 
+                sleep(0.01)
+            elif (angle>0):
+                self.State= self._turn(speed,False)# rotate CCW
+            else: 
+                self.State=self._turn(speed,True)# rotatte CW 
+            while (self.EncoderL.encoderCount <numPulses):
+                self.x_pos, self.y_pos, self.rot = self._updatePos()
+                sleep(0.02)
+            self._stop()
+
+            distance = sqrt(X**2+Y**2) # calculate distance to drive forward 
+            encoderOldCount = self.EncoderL.encoderCount # update encoder count 
+            numPulses = (distance/GLOBALSM1.distancePerPulse)+encoderOldCount # get the new final target pulses
+            self.State = self._forwards(speed) # drive forwards 
+            while (self.EncoderL.encoderCount <numPulses):# keep going forwards until you reach the desired number of pulses 
+                self.x_pos, self.y_pos, self.rot = self._updatePos()
+                sleep(0.02)
+            self._stop()
+            #Print out location reached based on encoders
+            self.x_pos, self.y_pos, self.rot = self._updatePos()
+            print(f'Reached {self.x_pos}, {self.y_pos} with rot of {self.rot}\n')
+
+        except KeyboardInterrupt:
+            self._exemptExit()
+    #Ball hitting functions 
+    def hitBallBasic(self)->None:
+        pauseTime = 0.2
+        noHit = True
+        try :
+            while (noHit): # while not close enough to ball 
+                (direction, temp, distance)= self.vision.detect() # run vision check 
+                if (direction == DIRECTION.Ahead): # if ball ahead
+                    speed = 100
+                    pauseTime = 0.5
+                    if (distance <0.4):
+                        speed = 20
+                        self.vision.tolerence = 100
+                        self.State = self._forwards(speed)
+                    if (distance <0.3): # if close to ball 
+                        self.State = self._forwards(30)
+                        sleep(4)
+                        self.x_pos, self.y_pos, self.rot = self._updatePos()
+                        noHit = False # end 
+                        self._stop()
+                    else: 
+                        self.State = self._forwards(speed) # move forward 
+                elif (direction == DIRECTION.CannotFind):
+                    speed = 20
+                    pauseTime = 0.3
+                    self.State = self._turn(speed,ANTICLOCKWISE)
+                elif (direction == DIRECTION.Left):
+                    speed = 20
+                    pauseTime =0.15
+                    self.State = self._turn(speed,ANTICLOCKWISE)
+
+                else:
+                    self.State = self._turn(speed,CLOCKWISE)
+                    speed = 20
+                    pauseTime =0.15
+                sleep(pauseTime)
+                self.x_pos, self.y_pos, self.rot = self._updatePos()
+
+        except KeyboardInterrupt:
+            self._exemptExit()
+        
 
 
 
