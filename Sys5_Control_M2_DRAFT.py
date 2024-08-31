1ef7df04d1dc4b4420bbb784fb8dcd0c6da1ee69
EVladicic <evla0003@student.monash.edu>
2024-08-31 11:32:49 +1000

    Major changes for control
    split functions into multiple subfunctions

--- Sys5_Control_M2_DRAFT.py
+++ Sys5_Control_M2_DRAFT.py
@@ -7,6 +7,8 @@ from Sys4_Vision import Sys4_Vision
 from time import sleep
 from math import pi, atan2, sqrt, sin, cos
 
+from typing import Tuple
+
 import RPi.GPIO as GPIO
 import GLOBALSM1 # for physical values of the robot i.e. wheel diameter, distance b/w wheeels etc   
 
@@ -67,7 +69,7 @@ class Sys5_Control:
         # define the initial position of the robot as (0,0) with a rotation of 0 
         self.x_pos =0
         self.y_pos = 0
-        self.rot =0 
+        self.rot = 0 
 
         self.State = None # State is the movement of the robot i.e. Left, Right, Forward, Backward
         # initalise to none bc robot is not moving
@@ -180,7 +182,7 @@ class Sys5_Control:
             y = y_old + dy 
             x = x_old + dx
 
-        elif self.State ==ACTION.BACKWARD:
+        elif self.State == ACTION.BACKWARD:
             rot = rot_old
             y = y_old - dy
             x = x_old - dx
@@ -202,3 +204,250 @@ class Sys5_Control:
             rot +=360 
 
         return x,y,rot
+
+    # fuction that calls the vision system and determines the direction that the robot needs to move 
+    # the distance to the ball and if the robot will hit the ball in the next move 
+    def hitBallSettings(self) -> Tuple[DIRECTION, float, float, bool]:
+
+        # INPUTS: self 
+
+        # OUTPUTS
+        # direction = direction that the robot is relative to the ball
+        # speed = speed that the robot should travel at in the next time step
+        # pauseTime = how long the robot should travel at this speed for 
+        # noHit = whether the ball will be hit by performing this movement 
+
+        (direction, temp, distance)= self.vision.detect() # run vision check 
+
+        if (direction == DIRECTION.Ahead):
+
+            # settings if robot if more than 0.55m away from the ball 
+            speed = 30 # set the drive speed 
+            pauseTime = 5 # how long in secs the robot should drive forwards for 
+
+            if (distance <  0.55): # if robot is less than 0.55 m from ball 
+                speed = 20 # reduce speed of robot
+            
+            if (distance < 0.40): # close to ball, drive forwards until you hit it
+                speed = 30 
+                pauseTime = 5.5 
+                noHit = False
+
+        elif (direction == DIRECTION.CannotFind):
+            speed = 20
+            pauseTime = 0.3
+
+        else: # ball is in field of view but is either left or right
+            speed = 15
+            pauseTime = 0.15 
+        
+        return direction, speed, pauseTime, noHit
+    
+    # function that implements the movement to hit the ball
+    def hitBall(self) -> None: 
+        noHit = True 
+        try:
+            while(noHit): # while not close enough to the ball
+
+                # get position of robot 
+                self.x_pos, self.y_pos, self.rot = self._updatePos(self.x_pos,self.y_pos,self.rot)
+                
+                # get the speed, pauseTime and if the robot will hit the ball in the next timestep
+                direction, speed, pauseTime, noHit = self.hitBallSettings() 
+
+                # Ball AHEAD
+                if (direction == DIRECTION.Ahead):
+                    self.State = self._forwards(speed)
+                
+                # Ball CANNOT FIND 
+                elif (direction == DIRECTION.CannotFind):
+                    self.State = self._turn(speed, ANTICLOCKWISE)
+                
+                # Ball LEFT 
+                elif (direction == DIRECTION.Left):
+                    self.State = self._turn(speed,ANTICLOCKWISE)
+                
+                # Ball RIGHT
+                else:
+                    self.State = self._turn(speed,CLOCKWISE)
+                
+                sleep(pauseTime) # do that movement for the designated n.o sec defined in PauseTime
+                self._stop() # stop movement of robot temporarily until next action is determined
+            
+            print(f'Reached {self.x_pos}, {self.y_pos} with rot of {self.rot}\n')
+        
+        except KeyboardInterrupt: 
+            self._exemptExit()
+        
+    #disengages from the ball so that it doesn't hit the ball incorrectly during the return to home sequence
+    def disEngage(self)->None:
+        
+        speed = 30 # set speed
+        self.x_pos, self.y_pos, self.rot = self._updatePos(self.x_pos,self.y_pos,self.rot) # determine OG pos
+
+        # Backs away from the ball for 3 seconds
+        self.State = self._backwards(speed)
+        sleep(3) 
+        self.x_pos, self.y_pos, self.rot = self._updatePos(self.x_pos,self.y_pos,self.rot)
+
+        # Re aligns the dolly wheel
+        self.State = self._forwards(speed)
+        sleep(0.5) 
+        self.x_pos, self.y_pos, self.rot = self._updatePos(self.x_pos,self.y_pos,self.rot)
+
+    # calculates the number of pulses required to achieve the desired turn and forwards direction
+    def EncoderPulseCalulator(self, angle, forward_distance) -> list[int]:
+        # INPUTS
+        # self 
+        # angle = desired angle rotation 
+        # forward_distance = required forward distance
+
+        # OUTPUTS
+        # angle_numPulses = number of angle pulse required to achieve desired rotation
+        # forward_numPulses = number of pulses required to achieve desired forwards distance
+
+    
+        # calculate the number of pulses required to achieve the turn 
+        angle_distance = GLOBALSM1.wheelBaseCircumference*abs(angle)/360
+        angle_numPulses = (angle_distance/GLOBALSM1.distancePerPulse) + self.EncoderL.encoderCount
+
+        # calculate the number of pulses required to achieve the distance required 
+        forward_numPulses = (forward_distance/GLOBALSM1.distancePerPulse) + self.EncoderL.encoderCount
+
+        return angle_numPulses, forward_numPulses
+    
+
+    def homeSettings(self) -> list[float]:
+
+        # calculate angle to return hom
+        angle = atan2(-self.y_pos,-self.x_pos)*180/pi
+        angle = angle - self.rot # make it relative to the current position of the robot
+
+        # calculate forward distance required to return home
+        forward_distance = sqrt(self.x_pos**2 + self.y_pos**2)
+
+        # calculate the number of pulses required to achieve the turn and the forward distance 
+        angle_numPulses, forward_numPulses = self.EncoderPulseCalulator(self, angle, forward_distance)
+
+        forward_numPulses = forward_numPulses*1.1 # add an offset to account for accumualted inaccuracies
+        
+        return angle, angle_numPulses, forward_distance, forward_numPulses
+    
+
+    def Home(self) -> None:
+        try:
+            speed = 30 # speed at which robot will move on its journey home
+            print(f' at X {self.x_pos}, Y {self.y_pos}, rot {self.rot}\n')
+
+            # determine the angle and forward distance required to return home and the associated 
+            # number of pulses required to achieve these movements
+            angle, angle_numPulses, forward_distance, forward_numPulses = self.homeSettings()
+
+            # rotate to achieve the desired angle 
+            if (angle >-1 and angle <1): # no rotation required 
+                sleep(0.01)
+
+            elif (angle>0):
+                self.State = self._turn(speed,ANTICLOCKWISE) # rotate CCW
+
+            else: 
+                self.State = self._turn(speed,CLOCKWISE) # rotate CW 
+
+            while (self.EncoderL.encoderCount <angle_numPulses):
+                self.x_pos, self.y_pos, self.rot = self._updatePos(self.x_pos, self.y_pos, self.rot)
+                sleep(0.02)
+
+            self._stop()
+
+
+            # drive forwards until you hit home 
+
+            self.State = self._forwards(speed)
+
+            while (self.EncoderL.encoderCount <forward_numPulses):# keep going fowards until you reach the desired number of pulses 
+                self.x_pos, self.y_pos, self.rot = self._updatePos(self.x_pos,self.y_pos,self.rot)
+                sleep(0.02)
+
+            self._stop
+            GPIO.cleanup()
+
+        except KeyboardInterrupt:
+            self._exemptExit()
+        
+
+    def searchPattern(self)-> None:
+
+        # define speed constants 
+        turn_speed = 15
+        forward_speed = 30
+        self.x_pos, self.y_pos, self.rot = self._updatePos(self.x_pos,self.y_pos,self.rot) # update position
+
+        try: 
+            (direction, temp, distance)= self.vision.detect() # run vision check 
+
+            while(direction == DIRECTION.CannotFind):
+                # turn anticlocwise, +ve in our coordinate system for 0.2 seconds 
+                self.State = self._turn(turn_speed, ANTICLOCKWISE) 
+                sleep(0.2)
+                self.x_pos, self.y_pos, self.rot = self._updatePos(self.x_pos,self.y_pos,self.rot) # update position 
+
+                if (self.rot >= 90): # if we have rotated more than 90 degrees 
+                    angle = - self.rot 
+                    forward_distance = 2 
+
+                    # call function that determines the number of pulses required to achieve desired movement 
+                    angle_numPulses, forward_numPulses = self.EncoderPulseCalulator(self, angle, forward_distance)
+
+                    # rotate to achieve the desired angle 
+                    if (angle >-1 and angle <1): # no rotation required 
+                        sleep(0.01)
+
+                    elif (angle>0):
+                        self.State = self._turn(turn_speed,ANTICLOCKWISE) # rotate CCW
+
+                    else: 
+                        self.State = self._turn(turn_speed,CLOCKWISE) # rotate CW 
+
+                    while (self.EncoderL.encoderCount <angle_numPulses):
+                        self.x_pos, self.y_pos, self.rot = self._updatePos(self.x_pos, self.y_pos, self.rot)
+                        sleep(0.02)
+
+                    self._stop()
+
+                    # drive forwards until you hit desired position
+
+                    self.State = self._forwards(forward_speed)
+
+                    while (self.EncoderL.encoderCount <forward_numPulses):# keep going fowards until you reach the desired number of pulses 
+                        self.x_pos, self.y_pos, self.rot = self._updatePos(self.x_pos,self.y_pos,self.rot)
+                        sleep(0.02)
+                    self._stop()
+
+                    #Print out location reached based on encoders
+                    print(f'Reached {self.x_pos}, {self.y_pos} with rot of {self.rot}\n')
+
+        except KeyboardInterrupt:
+            self._exemptExit() 
+
+
+
+if __name__ == "__main__":
+    robot = Sys5_Control() 
+    robot.vision.tolerence = 25
+    # tell robot to do stuff between here 
+    robot.SearchPattern()
+    robot.hitBallBetter()
+    robot.disEngage()
+    robot.Home()
+    
+    print(f'Finished {robot.x_pos}, {robot.y_pos} with rot of {robot.rot}\n') 
+    
+    #and here 
+    robot.release() #release motor pins 
+
+
+
+
+
+
+
